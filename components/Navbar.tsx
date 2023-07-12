afdde1da05e7140a6c959a614376bf9175cdfc9c
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-07-12 21:07:49 +0600

    profile photo added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,26 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header className="bg-dark text-light w-fit h-20 fixed bottom-10 left-1/2 -translate-x-1/2 flex justify-center items-center px-10 rounded-2xl">
       <nav>
-        <ul className="flex gap-10">
+        <ul className="flex items-center gap-10">
+          <li className="flex items-center">
+            <Link
+              href="/"
+              className="w-12 h-12 overflow-hidden rounded-full inline-block"
+            >
+              <Image
+                src="https://res.cloudinary.com/dpkbthpcw/image/upload/v1689173952/nextjs-portfolio/assets/profile/Profile_Photo_of_Mine_jn1jmc.jpg"
+                alt="Profile photo of Md. Shohanur Rahman"
+                width={50}
+                height={50}
+                priority
+                className="w-full h-full object-cover"
+              />
+            </Link>
+          </li>
           <li>
             <Link href="/">Home</Link>
           </li>
