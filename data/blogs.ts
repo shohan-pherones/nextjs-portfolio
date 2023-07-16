af8955df3d9eb38f7030732a88c1440c8951e92b
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-07-16 15:50:01 +0600

    blogs modified

--- data/blogs.ts
+++ data/blogs.ts
@@ -1,46 +1,41 @@
 export const data = [
   {
     id: 1,
-    title: "The Basics of HTML5",
-    author: "John Smith",
-    authorUrl: "https://www.example.com/johnsmith",
-    publishedDate: "May 20, 2023",
+    title:
+      "Regression.js: Simplifying Machine Learning Regression with JavaScript",
+    author: "Md. Shohanur Rahman",
+    authorUrl: "https://www.linkedin.com/in/shohan-pherones",
+    publishedDate: "July 16, 2023",
     image:
-      "https://images.pexels.com/photos/3109808/pexels-photo-3109808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    body: "In this blog post, we will explore the fundamentals of HTML5 and its key features. We'll cover the new semantic elements, form enhancements, multimedia support, and more.",
-    blogUrl: "https://www.example.com/blog/html5-basics",
+      "https://media.licdn.com/dms/image/D5612AQFDr7QdTwozFA/article-cover_image-shrink_423_752/0/1689500396404?e=1695254400&v=beta&t=vtqlDIBcgzxXxVe3a602GyI5kXYScY8D-vF4cCabyEU",
+    body: "In the world of machine learning, regression analysis plays a crucial role in predicting and understanding relationships between variables. Regression models are widely used across various industries for tasks such as sales forecasting, trend analysis, and risk assessment. JavaScript, being one of the most popular programming languages, offers a powerful library called Regression.js that simplifies the implementation of regression analysis, making it accessible to a broader audience of developers.",
+    blogUrl:
+      "https://www.linkedin.com/pulse/regressionjs-simplifying-machine-learning-regression-rahman/?trackingId=fr0yEIEsQy2LqUaH7iJhoA%3D%3D",
   },
   {
     id: 2,
-    title: "CSS Grid Layout: A Comprehensive Guide",
-    author: "Jane Doe",
-    authorUrl: "https://www.example.com/janedoe",
-    publishedDate: "May 20, 2023",
+    title:
+      "Understanding Color Theory in CSS: A Guide to Harmonious Web Design",
+    author: "Md. Shohanur Rahman",
+    authorUrl: "https://www.linkedin.com/in/shohan-pherones",
+    publishedDate: "June 20, 2023",
     image:
-      "https://images.pexels.com/photos/16545082/pexels-photo-16545082/free-photo-of-cup-of-coffee-with-a-daisy.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    body: "Discover the power of CSS Grid Layout in this comprehensive guide. Learn how to create complex, responsive layouts with ease, and explore various grid properties and techniques.",
-    blogUrl: "https://www.example.com/blog/css-grid-layout",
+      "https://media.licdn.com/dms/image/D5612AQGg_G_U4jyd6g/article-cover_image-shrink_600_2000/0/1687240286523?e=1695254400&v=beta&t=lM1vIC_79RPxcSIY2wDTUBpmpyFEQyQBwYZcsns7vgE",
+    body: "Color plays a vital role in web design, evoking emotions, conveying messages, and capturing users' attention. As a web developer or designer, having a solid grasp of color theory is crucial for creating visually appealing and harmonious websites. In this article, we'll explore the fundamentals of color theory and how to apply them using CSS.",
+    blogUrl:
+      "https://www.linkedin.com/pulse/understanding-color-theory-css-guide-harmonious-web-design-rahman/?trackingId=QZIPXD2%2FRO6i9v0suzZApQ%3D%3D",
   },
   {
     id: 3,
-    title: "JavaScript ES6: Exploring the Modern Features",
-    author: "David Williams",
-    authorUrl: "https://www.example.com/davidwilliams",
-    publishedDate: "May 20, 2023",
+    title:
+      "Choosing Between Express and Next.js: A Guide for Backend Development",
+    author: "Md. Shohanur Rahman",
+    authorUrl: "https://www.linkedin.com/in/shohan-pherones",
+    publishedDate: "June 3, 2023",
     image:
-      "https://images.pexels.com/photos/5581778/pexels-photo-5581778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    body: "JavaScript ES6 introduced many exciting features that revolutionized web development. Join us as we delve into arrow functions, template literals, destructuring, and other ES6 enhancements.",
-    blogUrl: "https://www.example.com/blog/js-es6-features",
-  },
-  {
-    id: 4,
-    title: "Optimizing Website Performance: Tips and Best Practices",
-    author: "Emily Johnson",
-    authorUrl: "https://www.example.com/emilyjohnson",
-    publishedDate: "May 20, 2023",
-    image:
-      "https://images.pexels.com/photos/9969154/pexels-photo-9969154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    body: "Improve your website's performance by implementing various optimization techniques. From optimizing images and minifying code to leveraging caching and CDNs, we cover it all in this blog post.",
-    blogUrl: "https://www.example.com/blog/website-performance-optimization",
+      "https://media.licdn.com/dms/image/D5612AQEmmFLOFBtvBQ/article-cover_image-shrink_600_2000/0/1685803051198?e=1695254400&v=beta&t=_K5t0QKmL57Ao5ddgp2aYnT4XJ3IUSLHs4V3G2BAL-k",
+    body: "When it comes to backend development in JavaScript, developers are often faced with the question of which framework to choose. Two popular options are Express and Next.js. Both frameworks have their strengths and weaknesses, and understanding their differences can help you make an informed decision. In this article, we will compare Express and Next.js, highlighting their key features and use cases to help you decide which framework is best suited for your backend development needs.",
+    blogUrl:
+      "https://www.linkedin.com/pulse/choosing-between-express-nextjs-guide-backend-md-shohanur-rahman/?trackingId=QZIPXD2%2FRO6i9v0suzZApQ%3D%3D",
   },
 ];
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "images.pexels.com"],
+    domains: ["res.cloudinary.com", "images.pexels.com", "media.licdn.com"],
   },
 };
 
