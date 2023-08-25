e6f4d6f035b03f8053620e090b76567f2aecb19a
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-08-26 02:41:14 +0600

    resume link updated

--- constants/resume.ts
+++ constants/resume.ts
@@ -1,2 +1,2 @@
 export const RESUME_LINK =
-  "https://drive.google.com/uc?export=download&id=1CE2QC9emx-Puul1RcvXbDbgT9ZV9FAk1";
+  "https://drive.google.com/uc?export=download&id=1nMjLhxfQL-nE1f1e4oAiVcJqOG0sBw1S";
