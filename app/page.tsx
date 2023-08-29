5ff2e861832af37c948a7ab44eb58055ee55341e
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-08-29 13:36:31 +0600

    messenger bot removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import About from "@/components/About";
 import Blogs from "@/components/Blogs";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
-import MessengerChatBot from "@/components/MessengerChatBot";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
@@ -15,7 +14,6 @@ const HomePage = () => {
       <Skills />
       <Blogs />
       <Contact />
-      <MessengerChatBot />
     </main>
   );
 };
