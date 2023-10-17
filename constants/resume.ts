9ad40d49f9a31a5fa144d3d45f6eb4732768fc50
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-10-17 13:10:49 +0600

    resume link updated

--- constants/resume.ts
+++ constants/resume.ts
@@ -1,2 +1,2 @@
 export const RESUME_LINK =
-  "https://drive.google.com/uc?export=download&id=15_g0vsPlSfNy3fOh4FP6YTxPYe6GMUkK";
+  "https://drive.google.com/uc?export=download&id=1IQwthmpok4kB-Qi5XBGRlXVdkaSenw5C";
--- data/blogs.ts
+++ data/blogs.ts
@@ -7,7 +7,7 @@ export const data = [
     authorUrl: "https://www.linkedin.com/in/shohan-pherones",
     publishedDate: "July 16, 2023",
     image:
-      "https://media.licdn.com/dms/image/D5612AQFDr7QdTwozFA/article-cover_image-shrink_423_752/0/1689500396404?e=1695254400&v=beta&t=vtqlDIBcgzxXxVe3a602GyI5kXYScY8D-vF4cCabyEU",
+      "https://media.licdn.com/dms/image/D5612AQFDr7QdTwozFA/article-cover_image-shrink_423_752/0/1689500396404?e=1703116800&v=beta&t=BDA_P9yFCuX_TJ_bBHBaTPt4ZS4xQ6_jYNf9AzzGLAo",
     body: "In the world of machine learning, regression analysis plays a crucial role in predicting and understanding relationships between variables. Regression models are widely used across various industries for tasks such as sales forecasting, trend analysis, and risk assessment. JavaScript, being one of the most popular programming languages, offers a powerful library called Regression.js that simplifies the implementation of regression analysis, making it accessible to a broader audience of developers.",
     blogUrl:
       "https://www.linkedin.com/pulse/regressionjs-simplifying-machine-learning-regression-rahman/?trackingId=fr0yEIEsQy2LqUaH7iJhoA%3D%3D",
@@ -20,7 +20,7 @@ export const data = [
     authorUrl: "https://www.linkedin.com/in/shohan-pherones",
     publishedDate: "June 20, 2023",
     image:
-      "https://media.licdn.com/dms/image/D5612AQGg_G_U4jyd6g/article-cover_image-shrink_600_2000/0/1687240286523?e=1695254400&v=beta&t=lM1vIC_79RPxcSIY2wDTUBpmpyFEQyQBwYZcsns7vgE",
+      "https://media.licdn.com/dms/image/D5612AQGg_G_U4jyd6g/article-cover_image-shrink_600_2000/0/1687240286523?e=1703116800&v=beta&t=F7ACxrigQP07aTgpnbDRIs0SlYaMOdOWQdJuM2KBpdY",
     body: "Color plays a vital role in web design, evoking emotions, conveying messages, and capturing users' attention. As a web developer or designer, having a solid grasp of color theory is crucial for creating visually appealing and harmonious websites. In this article, we'll explore the fundamentals of color theory and how to apply them using CSS.",
     blogUrl:
       "https://www.linkedin.com/pulse/understanding-color-theory-css-guide-harmonious-web-design-rahman/?trackingId=QZIPXD2%2FRO6i9v0suzZApQ%3D%3D",
@@ -33,7 +33,7 @@ export const data = [
     authorUrl: "https://www.linkedin.com/in/shohan-pherones",
     publishedDate: "June 3, 2023",
     image:
-      "https://media.licdn.com/dms/image/D5612AQEmmFLOFBtvBQ/article-cover_image-shrink_600_2000/0/1685803051198?e=1695254400&v=beta&t=_K5t0QKmL57Ao5ddgp2aYnT4XJ3IUSLHs4V3G2BAL-k",
+      "https://media.licdn.com/dms/image/D5612AQEmmFLOFBtvBQ/article-cover_image-shrink_600_2000/0/1685803051198?e=1703116800&v=beta&t=iWnsbLEECfbBMDDXyKaBCcPlg5UEGxPUF8ERZVmSs_U",
     body: "When it comes to backend development in JavaScript, developers are often faced with the question of which framework to choose. Two popular options are Express and Next.js. Both frameworks have their strengths and weaknesses, and understanding their differences can help you make an informed decision. In this article, we will compare Express and Next.js, highlighting their key features and use cases to help you decide which framework is best suited for your backend development needs.",
     blogUrl:
       "https://www.linkedin.com/pulse/choosing-between-express-nextjs-guide-backend-md-shohanur-rahman/?trackingId=QZIPXD2%2FRO6i9v0suzZApQ%3D%3D",
