8e5e682234ea1ff61c35a32ba69160366c05b243
shohan-pherones <shohan.sub.56@gmail.com>
2025-02-08 20:32:54 +0600

    resume link updated

--- constants/resume.ts
+++ constants/resume.ts
@@ -1,2 +1,2 @@
 export const RESUME_LINK =
-  'https://drive.google.com/uc?export=download&id=1MdZfEbTc-qk2CIxH_XZNAPe0WUUQprrP';
+  'https://drive.google.com/uc?export=download&id=1CJsJIkUXrWI4zqZ3b8FCd5mJeDas7LQ1';
